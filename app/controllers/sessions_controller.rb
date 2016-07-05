8d5f01f0bebe039628e59e821516376ba667b6fd
Dylan Huang <dylan.p.huang@gmail.com>
2016-07-04 17:25:52 -0700

    8.1.3 Done - Added login view using form_for and basic authentication in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def destroy
+  end
+
+  def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+          # login
+      else
+          render 'new'
+          # render error message
+      end
+  end
 end
